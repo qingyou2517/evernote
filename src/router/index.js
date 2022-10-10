2337c32f9441a6ecbbf082dbd7db738e0eb872ac
青游 <2517789608@qq.com>
2022-10-10 20:22:43 +0800

    新增路由与页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
+import Notebooks from '@/components/NotebookList.vue'
+import NoteDetail from '@/components/NoteDetail.vue'
+import TrashDetail from '@/components/TrashDetail.vue'
 
 Vue.use(Router)
 
@@ -14,8 +17,19 @@ export default new Router({
     },
     {
       path: '/login',
-      name: 'Login',
       component: Login
     },
+    {
+      path: '/notebooks',
+      component: Notebooks
+    },
+    {
+      path: '/note/:noteId',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      component: TrashDetail
+    }
   ]
 })
