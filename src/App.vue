b4847c6e4a927a841f249b5c74e4d693fefd39d4
青游 <2517789608@qq.com>
2022-10-10 21:40:08 +0800

    新增Sidebar组件，并引入svg

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,61 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <sidebar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import sidebar from './components/Sidebar'
+
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    sidebar
+  }
 }
 </script>
 
 <style>
-#app {
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+html, body, #app {
+  height: 100%;
+}
+
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 14px;
+  background-color: #eee;
+}
+
+a {
+  text-decoration: none;
+  color: #444;
+}
+
+ul, li {
+  list-style: none;
+}
+
+.btn {
+  color: #666;
+  font-size: 12px;
+  padding: 2px 4px;
+  background-color: #fff;
+  box-shadow: 0px 0px 2px 0px #ccc;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+}
+
+#app {
+  display: flex;
+  align-items: stretch;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="sidebar">
+    <div class="icons">
+      <router-link to="/note/1" title="笔记">
+        <svg class="icon">
+          <use xlink:href="#icon-note"></use>
+        </svg>
+      </router-link>
+      <router-link to="/notebooks" title="笔记本">
+        <svg class="icon">
+          <use xlink:href="#icon-notebook"></use>
+        </svg>
+      </router-link>
+      <router-link to="/trash/2" title="回收站">
+        <svg class="icon">
+          <use xlink:href="#icon-trash"></use>
+        </svg>
+      </router-link>
+    </div>
+    <div class="logout" @click="onLogout">
+      <svg class="icon">
+        <use xlink:href="#icon-logout"></use>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    onLogout() {
+      console.log('登出')
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+#sidebar {
+  position: relative;
+  width: 56px;
+  text-align: center;
+  background-color: #add8e6;
+}
+
+.icons {
+  margin-top: 30px;
+}
+
+.icons a {
+  padding: 6px 0;
+  display: block;
+}
+/*点击的 router-link 背景高亮*/
+.icons .router-link-active {
+  background: #fff;
+}
+
+.icon {
+  width: 1.3em;
+  height: 1.3em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+  margin-top: 2px;
+}
+
+.logout {
+  position: absolute;
+  bottom: 50px;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+}
+
+.logout > .icon {
+  width: 1.5em;
+  height: 1.5em;
+}
+
+.iconfont {
+  color: #fff;
+}
+</style>
