569bf2ccf83ae76c371d26c9098020b1f55c17ae
青游 <2517789608@qq.com>
2022-10-15 22:52:23 +0800

    笔记本列表静态页面

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,14 +1,46 @@
 <template>
-  <div id="notebook-list">
-    <h1>{{ msg }}</h1>
-    <ul>
-      <li>
-        <router-link to="/note/1">笔记本1</router-link>
-      </li>
-      <li>
-        <router-link to="/note/2">笔记本2</router-link>
-      </li>
-    </ul>
+  <div class="detail" id="notebook-list">
+    <header>
+      <a href="#" class="btn">
+        <svg class="icon">
+          <use xlink:href="#icon-add"></use>
+        </svg>
+        <span class="text">新建笔记本</span>
+      </a>
+    </header>
+    <main>
+      <div class="layout">
+        <h3>笔记本列表(10)</h3>
+        <div class="book-list">
+          <a href="#" class="notebook">
+            <div>
+              <svg class="icon">
+                <use xlink:href="#icon-notebook"></use>
+              </svg>
+              <h3>笔记本标题1</h3>
+              <span>3</span>
+              <span class="action">编辑</span>
+              <span class="action">删除</span>
+              <span class="date">3天前</span>
+            </div>
+          </a>
+          <a href="#" class="notebook">
+            <div>
+              <svg class="icon">
+                <use xlink:href="#icon-notebook"></use>
+              </svg>
+              <h3>笔记本标题2</h3>
+              <span>1</span>
+              <span class="action">编辑</span>
+              <span class="action">删除</span>
+              <span class="date">5天前</span>
+            </div>
+          </a>
+        </div>
+      </div>
+
+    </main>
+
   </div>
 </template>
 
@@ -18,7 +50,7 @@ import Auth from '../apis/auth'
 export default {
   data() {
     return {
-      msg: '笔记本列表'
+      msg:'笔记本列表'
     }
   },
   created() {
@@ -28,12 +60,11 @@ export default {
           this.$router.push({path: '/login'})
         }
       })
-  }
+  },
 }
 </script>
 
-<style scoped>
-h1 {
-  color: red;
-}
+<style lang="less" scoped>
+@import url(../assets/css/notebook-list.less);
+
 </style>
