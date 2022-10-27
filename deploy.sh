89e446e714153fe16812a10169bf96c3ced293f4
青游 <2517789608@qq.com>
2022-10-27 17:15:27 +0800

    部署到gitee

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:qingyou2517/evernote-website.git &&
+git remote add origin git@gitee.com:qilling/evernote-website.git &&
 git push -f -u origin master &&
 cd -
